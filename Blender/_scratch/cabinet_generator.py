7f379a9f3f4266373505044b07a63a076183721a
levans88 <lenny.evans3@gmail.com>
2019-06-27 07:17:22 -0500

    Testing mesh generation in Blender with python

---
+++ Blender/_scratch/cabinet_generator.py
@@ -0,0 +1,17 @@
+# https://www.youtube.com/watch?v=gVUvnSJ-t3M
+import bpy
+
+# float triplets each representing (X, Y, Z)
+verts = [(0,0,0),(0,5,0),(5,5,0),(5,0,0),(0,0,5),(0,5,5),(5,5,5),(5,0,5)]
+
+faces = [(0,1,2,3),(7,6,5,4),(0,4,5,1),(1,5,6,2),(2,6,7,3),(3,7,4,0)]
+
+mymesh = bpy.data.meshes.new("Cube")
+myobject = bpy.data.objects.new("Cube", mymesh)
+
+myobject.location = bpy.context.scene.cursor_location
+bpy.context.scene.objects.link(myobject)
+
+# from_pydata(vertices, edges, faces)
+mymesh.from_pydata(verts,[],faces)
+mymesh.update(calc_edges=True)
